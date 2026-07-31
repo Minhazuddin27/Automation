f11655ac2f545f8119ba5ae31e2709b804a168cf
asus <asus@192.168.1.33>
2026-07-31 17:24:16 +0530

    I am sending the Message

--- Testmaven/src/test/java/testpackage/GitTest.java
+++ Testmaven/src/test/java/testpackage/GitTest.java
@@ -3,8 +3,8 @@ package testpackage;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("YOU ARE WINNER");
-		System.out.println("checking 22");
+		System.out.println("YOU ARE WINNER Minhaz");
+		
 	}
 
 }
