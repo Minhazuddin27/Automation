18f952c288d06e2c6513ca98115cf7da4a80bbf7
Minhazuddin27 <shaikminhaz27@gmail.com>
2026-07-31 17:55:33 +0530

    Update GitTest.java

--- Testmaven/src/test/java/testpackage/GitTest.java
+++ Testmaven/src/test/java/testpackage/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("YOU ARE WINNER");
 		System.out.println("checking 22");
-		System.out.println("checking 22");
+		
 
 	}
 
