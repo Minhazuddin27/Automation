ff891cbb4aaa1b5d9806860464ef9957ff415c93
Minhazuddin27 <shaikminhaz27@gmail.com>
2026-08-04 12:25:43 +0530

    deleted the java file

--- Testmaven/src/test/java/testpackage/TestWebDriverManager.java
+++ Testmaven/src/test/java/testpackage/TestWebDriverManager.java
@@ -10,7 +10,7 @@ public class TestWebDriverManager {
 	public static void main(String[] args) {
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\asus\\eclipse-workspace\\chromedriver.exe");
 		WebDriverManager.chromedriver().setup(); // execute on chrome
-		// here we no need to add path all finish here
+		// here we no need to add path all finish her
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://testautomationpractice.blogspot.com/");
 
